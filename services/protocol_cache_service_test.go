d4c3a61d9d0f9e5ed1868e342f192114260dd058
Guilherme Biff Zarelli <guilherme.zarelli@luizalabs.com>
2021-12-15 17:13:01 -0300

    test: add test to cache service

--- services/protocol_cache_service_test.go
+++ services/protocol_cache_service_test.go
@@ -41,7 +41,45 @@ func TestGenerateNewProtocolWithoutExistsInCache(t *testing.T) {
 	pServiceMock.AssertNumberOfCalls(t, "NewProtocol", 1)
 }
 
-//TODO: Test recursive method
+func TestGenerateNewProtocolWithExistsInCache(t *testing.T) {
+	t.Run("with one retry", func(t *testing.T) {
+		asserts := assert.New(t)
+		cacheService, cacheMock, pServiceMock := prepareServicesAndMocks()
+		existedProtocol := "123456789"
+		expectedProtocol := "123456789-1"
+
+		cacheMock.ExpectExists(existedProtocol).SetVal(1)
+		cacheMock.ExpectExists(expectedProtocol).SetVal(0)
+		cacheMock.ExpectSet(expectedProtocol, true, Day).SetVal(CacheSuccessResult)
+		pServiceMock.On("NewProtocol").Return(existedProtocol, nil)
+
+		protocol, err := cacheService.NewProtocol()
+
+		asserts.Equal(expectedProtocol, protocol)
+		asserts.Nil(err)
+		pServiceMock.AssertNumberOfCalls(t, "NewProtocol", 2)
+	})
+	t.Run("with four retry", func(t *testing.T) {
+		asserts := assert.New(t)
+		cacheService, cacheMock, pServiceMock := prepareServicesAndMocks()
+		existedProtocol := "123456789"
+		expectedProtocol := "123456789-4"
+
+		cacheMock.ExpectExists(existedProtocol).SetVal(1)
+		for i := 1; i < 4; i++ {
+			cacheMock.ExpectExists(fmt.Sprintf("%s-%d", existedProtocol, i)).SetVal(1)
+		}
+		cacheMock.ExpectExists(expectedProtocol).SetVal(0)
+		cacheMock.ExpectSet(expectedProtocol, true, Day).SetVal(CacheSuccessResult)
+		pServiceMock.On("NewProtocol").Return(existedProtocol, nil)
+
+		protocol, err := cacheService.NewProtocol()
+
+		asserts.Equal(expectedProtocol, protocol)
+		asserts.Nil(err)
+		pServiceMock.AssertNumberOfCalls(t, "NewProtocol", 5)
+	})
+}
 
 func prepareServicesAndMocks() (ProtocolService, redismock.ClientMock, *MockProtocolService) {
 	pServiceMock := new(MockProtocolService)
